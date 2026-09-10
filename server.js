73dd9c44e99343201ff8fb830955b86794a427bb
nulxd! <intelcore123456f@proton.me>
2026-09-10 17:36:00 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,46 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const ROOT = __dirname;
+const PORT = process.env.PORT || 8080;
+const MIME = {
+  ".html": "text/html; charset=utf-8",
+  ".js": "text/javascript; charset=utf-8",
+  ".css": "text/css; charset=utf-8",
+  ".png": "image/png",
+  ".jpg": "image/jpeg",
+  ".jpeg": "image/jpeg",
+  ".gif": "image/gif",
+  ".svg": "image/svg+xml",
+  ".swf": "application/x-shockwave-flash",
+  ".mp4": "video/mp4",
+  ".ico": "image/x-icon",
+  ".json": "application/json",
+  ".woff": "font/woff",
+  ".woff2": "font/woff2",
+};
+
+http
+  .createServer((req, res) => {
+    let urlPath = decodeURIComponent(req.url.split("?")[0] || "/");
+    if (urlPath === "/") urlPath = "/index.html";
+
+    const filePath = path.normalize(path.join(ROOT, urlPath));
+    if (!filePath.startsWith(ROOT)) {
+      res.writeHead(403);
+      return res.end("Forbidden");
+    }
+
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        return res.end("404 Not Found: " + urlPath);
+      }
+      res.writeHead(200, { "Content-Type": MIME[path.extname(filePath)] || "application/octet-stream" });
+      res.end(data);
+    });
+  })
+  .listen(PORT, () => {
+    console.log("vlPlayer test server running at http://localhost:" + PORT + "/");
+  });
